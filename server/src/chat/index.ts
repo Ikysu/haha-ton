9c908793ba581ad92be9a02fc14a757c8131c81f
DamirLut <d.amlutstudio@gmail.com>
2022-11-19 09:10:17 +0300

    залил клиента

--- server/src/chat/index.ts
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from "sequelize";
-import { Socket } from "socket.io";
-
-export function connect(socket: Socket, db: Sequelize) {
-    
-}
--- server/src/routers/packages/update.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import { Sequelize } from "sequelize";
-
-
-export default async function (req: FastifyRequest, reply: FastifyReply, db: Sequelize) {
-    reply.send({ok:false})
-    
-}
