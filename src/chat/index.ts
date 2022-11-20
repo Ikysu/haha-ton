8b4707e1cf94836eb2806fb68471936f13e1cd64
ikysu <kyzminpro@gmail.com>
2022-11-20 05:49:27 +0500

    backup

--- src/chat/index.ts
+++ src/chat/index.ts
@@ -5,8 +5,13 @@ let rooms = {
 
 }
 
+type ConnectionData = {
+    uid:string; // package uid
+    token:string; // user token
+}
+
 export function connection(socket: Socket, db: Sequelize) {
-    socket.on("room-connect", (token: string)=>{
-        console.log(token)
+    socket.on("room-connect", (data: ConnectionData)=>{
+        console.log(data)
     })
 }
